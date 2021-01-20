da015cb0815758a06fc9f0ce3b972969c33a623e
ervir <erviradwilestari9@gmail.com>
2021-01-20 12:45:23 +0700

    Thirth Coommit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 class Hello{
 	public static void main(String[] args){
+		System.out.println("Judul");
 		System.out.println("Ervir: Belajar Github Aplikasi Java 2021");
+		System.out.println("--- *** ---");
+		
 	}
 }
